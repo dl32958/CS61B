c8e7c976495869dc85e348eeaf809c0361e6c732
Dong Lu <144203360+dl32958@users.noreply.github.com>
2023-11-02 10:45:05 -0700

    proj0/ update

--- proj0/src/aoa/choosers/EvilChooser.java
+++ proj0/src/aoa/choosers/EvilChooser.java
@@ -8,13 +8,25 @@ public class EvilChooser implements Chooser {
     private String pattern;
     private List<String> wordPool;
 
+    /** Always choose the family with the largest number of words.
+     *  Always choose the family with the pattern that comes alphabetically earlier. */
     public EvilChooser(int wordLength, String dictionaryFile) {
         // TODO: Fill in this constructor.
+        if (wordLength < 1){
+            throw new IllegalArgumentException();
+        }
+        wordPool = FileUtils.readWordsOfLength(dictionaryFile, wordLength);
+        int numWords = wordPool.size();
+        if (numWords == 0){
+            throw new IllegalStateException();
+        }
     }
 
     @Override
     public int makeGuess(char letter) {
         // TODO: Fill in this method.
+        /** find all the possible word families split on the guessed letter
+         *  and pick the one with the most words. */
         return 0;
     }
 
